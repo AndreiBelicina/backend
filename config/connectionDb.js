2106a63736ecbdcf26d6b4b9269cc81051e415ae
Belicina, Andrei Lester <andreilester.belicina15@gmail.com>
2025-01-20 20:35:36 +0800

    erw

--- config/connectionDb.js
+++ config/connectionDb.js
@@ -1,8 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-const connectDb = async() => {
-    await mongoose.connect(process.env.MONGODB_URI)
-    .then(()=> console.log("connected..."))
-}
+const connectDb = async () => {
+  try {
+    const uri = process.env.MONGO_URI; // Ensure this matches the variable name in your environment
+    if (!uri) {
+      throw new Error("MongoDB connection URI is not defined in the environment variables.");
+    }
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the application if the connection fails
+  }
+};
 
-module.exports = connectDb
+module.exports = connectDb;
